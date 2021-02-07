524f019a6e9d43c3eaae7dae796ac188052d9644
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-02-07 14:31:17 -0500

    created db and config folders

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Insert a set of records.
+INSERT INTO burgers (id, burger_name, devoured) VALUES (1, 'cheeseburger' , true);
+
+INSERT INTO burgers (id, burger_name, devoured) VALUES (2, 'reuben' , true);
+
+INSERT INTO burgers (id, burger_name, devoured) VALUES (3, 'baconator' , false);
